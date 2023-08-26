27005e8e1303eff35b571011753f0073e462419e
Katherine <kbenjumeao@eafit.edu.co>
2023-08-25 20:49:44 -0500

    avance

--- Reto 2/Gateway/src/clientGrpc.py
+++ Reto 2/Gateway/src/clientGrpc.py
@@ -7,6 +7,8 @@ SERVER_ADDRESS = os.getenv("HOST_GRPC")
 SERVER_PORT = os.getenv("PORT_GRPC")
 
 def list_files():
+    print(SERVER_ADDRESS)
+    print(SERVER_PORT)
     with grpc.insecure_channel(f'{SERVER_ADDRESS}:{SERVER_PORT}') as channel:
         for file in files_pb2_grpc.FilesStub(channel).GetFilesList(files_pb2.ListFilesRequest()).files:
             print(file.filename)
